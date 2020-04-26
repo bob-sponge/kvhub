837055cb862f31d65b5df6fddee12e11782fd957
lw <819740451@qq.com>
2020-04-26 21:29:55 +0800

    feat: namespace detail function

--- packages/server/src/modules/namespace/namespace.controller.ts
+++ packages/server/src/modules/namespace/namespace.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { NamespaceService } from './namespace.service';
 import { ResponseBody } from 'src/vo/ResponseBody';
 import { NamespaceViewDetail } from 'src/vo/NamespaceViewDetail';
-import { NamespaceKey } from 'src/vo/NamespaceKey';
 
 @Controller('namespace')
 export class NamespaceController {
@@ -10,7 +9,14 @@ export class NamespaceController {
   /**
    * 根据namespaceId, languageId,KeyTranslateProgressStatus(all, unfinished, finished)
    * 分页返回,查询条件为keyname
-   * @param namespaceViewDetail:
+   * @param namespaceViewDetail
+   * namespaceId:1
+   * referenceLanguageId:1
+   * targetLanguageId:2
+   * KeyTranslateProgressStatus:all
+   * page:1
+   * pageSize:8
+   * condition:n
    * @returns
    * {
     "statusCode": 0,
