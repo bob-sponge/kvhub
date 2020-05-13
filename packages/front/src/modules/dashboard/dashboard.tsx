48f478d9852e848d75c5f99f646d15d508c957b7
z00404wu <si-wen.bian@siemens.com>
2020-05-13 15:16:25 +0800

    feat: add interface in dashboard && add project

--- packages/front/src/modules/dashboard/dashboard.tsx
+++ packages/front/src/modules/dashboard/dashboard.tsx
@@ -1,84 +1,107 @@
 import React, { useState, useEffect } from 'react';
 import * as css from './style/dashboard.modules.less';
-import { Button, Progress } from 'antd';
+import { Button, Progress, Empty, Spin } from 'antd';
 import { UploadOutlined, PlusOutlined } from '@ant-design/icons';
 import Container from '../../container';
-import { cardListData, doneColor, processColor, formatNumber, timeAgo } from './constant';
+import { doneColor, processColor, formatNumber, timeAgo } from './constant';
 import AddOrEditProject from './addOrEditProject';
 import { ajax } from '@ofm/ajax';
 
 const Dashboard: React.SFC = () => {
   const [visible, setVisible] = useState<boolean>(false);
+  const [projectList, setProjectList] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    ajax.get('/project/dashboard/all').then(result => {
-      window.console.log(result);
-    });
+    getProjectAll();
   }, []);
+
+  const getProjectAll = () => {
+    setLoading(true);
+    ajax
+      .get('/project/dashboard/all')
+      .then(result => {
+        setLoading(false);
+        const {
+          data: { statusCode, data },
+        } = result;
+        if (statusCode === 0) {
+          setProjectList(data);
+        }
+      })
+      .catch(error => {
+        if (error) {
+          setLoading(false);
+        }
+      });
+  };
 
   const addProject = () => {
     setVisible(true);
   };
   return (
-    <Container>
-      <div>
-        <div className={css.dashboardTitle}>
-          <div className={css.title}>Project Dashboard</div>
-          <div className={css.operation}>
-            <Button type="primary" ghost icon={<UploadOutlined />}>
-              secondary
-            </Button>
-            <Button type="primary" icon={<PlusOutlined />} onClick={addProject}>
-              Add Project
-            </Button>
+    <Spin spinning={loading}>
+      <Container>
+        <div>
+          <div className={css.dashboardTitle}>
+            <div className={css.title}>Project Dashboard</div>
+            <div className={css.operation}>
+              <Button type="primary" ghost icon={<UploadOutlined />}>
+                Upload
+              </Button>
+              <Button type="primary" icon={<PlusOutlined />} onClick={addProject}>
+                Add Project
+              </Button>
+            </div>
           </div>
-        </div>
-        <div className={css.dashboardContent}>
-          {cardListData &&
-            cardListData.length > 0 &&
-            cardListData.map(item => {
-              const isDone = item.translateKeysNumber === item.keysNumber;
-              const precent = (item.translateKeysNumber / item.keysNumber) * 100;
-              return (
-                <div className={css.cardWapper}>
-                  <div className={css.cardList}>
-                    <div className={css.cardTitle}>{item.name}</div>
-                    <div className={css.cardTranslate}>
-                      <div className={css.keys}>
-                        <div className={css.keysCurrent} style={{ color: isDone ? doneColor : processColor }}>
-                          {formatNumber(item.translateKeysNumber)}
+          <div className={css.dashboardContent}>
+            {projectList &&
+              projectList.length > 0 &&
+              projectList.map((item, index) => {
+                const isDone = item.translateKeysNumber === item.keysNumber;
+                const precent = (item.translateKeysNumber / item.keysNumber) * 100;
+                return (
+                  <div className={css.cardWapper} key={index}>
+                    <div className={css.cardList}>
+                      <div className={css.cardTitle}>{item.name}</div>
+                      <div className={css.cardTranslate}>
+                        <div className={css.keys}>
+                          <div className={css.keysCurrent} style={{ color: isDone ? doneColor : processColor }}>
+                            {formatNumber(item.translateKeysNumber)}
+                          </div>
+                          <div className={css.keysTotal}>{formatNumber(item.keysNumber)}</div>
                         </div>
-                        <div className={css.keysTotal}>{formatNumber(item.keysNumber)}</div>
+                        <div className={css.precent} style={{ color: isDone ? doneColor : processColor }}>
+                          <span>{isDone ? 'Done 100' : precent.toFixed(2)}</span>
+                        </div>
                       </div>
-                      <div className={css.precent} style={{ color: isDone ? doneColor : processColor }}>
-                        <span>{isDone ? 'Done 100' : precent.toFixed(2)}</span>
+                      <div className={css.cardBar}>
+                        <Progress
+                          percent={precent}
+                          size="small"
+                          strokeColor={isDone ? doneColor : processColor}
+                          showInfo={false}
+                        />
                       </div>
-                    </div>
-                    <div className={css.cardBar}>
-                      <Progress
-                        percent={precent}
-                        size="small"
-                        strokeColor={isDone ? doneColor : processColor}
-                        showInfo={false}
-                      />
-                    </div>
-                    <div className={css.cardUpdate}>
-                      <div className={css.language}>
-                        {item.languages.map((list: string) => {
-                          return <span>{list}</span>;
-                        })}
-                        {` ${item.languages.length} languages`}
+                      <div className={css.cardUpdate}>
+                        <div className={css.language}>
+                          {item.languages.map((list: string) => {
+                            return <span>{list}</span>;
+                          })}
+                          {` ${item.languages.length} languages`}
+                        </div>
+                        <div>{timeAgo(item.time)}</div>
                       </div>
-                      <div>{timeAgo(item.time)}</div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+          </div>
+          {projectList.length === 0 && <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />}
+          <AddOrEditProject visible={visible} setVisible={setVisible} getProjectAll={getProjectAll} />
         </div>
-        <AddOrEditProject visible={visible} setVisible={setVisible} />
-      </div>
-    </Container>
+      </Container>
+    </Spin>
   );
 };
 
