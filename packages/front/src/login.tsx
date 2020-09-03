4336d328ff36017b636df236e8b4adeefa7002d7
xiaoxiaoting <1062764645@qq.com>
2020-09-03 14:42:09 +0800

    feat: optimize login page

--- packages/front/src/login.tsx
+++ packages/front/src/login.tsx
@@ -2,6 +2,8 @@ import React, { useCallback } from 'react';
 import { Input, Form, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import * as css from '../src/style/login.modules.less';
+const titleLogo = require('./resource/logo_smz.png');
+const i18nLogo = require('./resource/logo-i18n.png');
 
 interface LoginProps {}
 
@@ -17,8 +19,13 @@ const Login: React.FC<LoginProps> = () => {
 
   return (
     <>
+      <img className={css.logo} src={titleLogo}></img>
       <div className={css.loginPage}>
         <div className={css.loginForm}>
+          <div className={css.i18N}>
+            <img className={css.i18NLogo} src={i18nLogo}></img>
+          </div>
+          <div className={css.label}>INTERNATIONALIZATION</div>
           <Form form={form} layout="vertical" hideRequiredMark>
             <div>
               <Form.Item name={'username'} rules={[{ required: true, message: 'Please enter user name' }]}>
@@ -29,12 +36,13 @@ const Login: React.FC<LoginProps> = () => {
               </Form.Item>
             </div>
           </Form>
-          <div>
+          <div className={css.loginBtn}>
             <Button onClick={handleSubmit} type="primary">
               Login
             </Button>
           </div>
         </div>
+        <div className={css.footer}>© Siemens AG, 2009 - 2020 All Rights Reserved</div>
       </div>
     </>
   );
